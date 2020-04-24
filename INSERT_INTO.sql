7bdb18016e29c46ff35c2e0e039ff66f7247173f
git4anirudha <64259508+git4anirudha@users.noreply.github.com>
2020-04-24 20:31:23 +0530

    DB Files

---
+++ INSERT_INTO.sql
@@ -0,0 +1,10 @@
+USE [AdventureWorks2014]
+GO
+
+INSERT INTO [dbo].[DEPT]
+           ([DEPTNO]
+           ,[DNAME]
+           ,[LOC])
+     VALUES
+           (55,'ANI', 'MUN')
+GO
---
+++ SELECT_DEPT.sql
@@ -0,0 +1,8 @@
+USE [AdventureWorks2014]
+GO
+
+SELECT [DEPTNO]
+      ,[DNAME]
+      ,[LOC]
+  FROM [dbo].[DEPT]
+GO
